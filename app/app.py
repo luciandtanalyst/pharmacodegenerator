e2edf768c2c202ec28e2a5a431c6fac05fc07d6f
Lucian Florea <lucianfl80@gmail.com>
2026-07-03 13:12:48 +0100

    update the project structure

---
+++ app/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    print("Hello from app pharmacodegenerator!")
+
+
+if __name__ == "__main__":
+    main()
--- app/code_generator.py
+++ app/code_generator.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from pharmacodegenerator!")
+    print("Hello from code generator!")
 
 
 if __name__ == "__main__":
