69695f02ad1180b1c537a72ddb5c55bc336f8ef0
LucianDTAnalyst <luciandtanalyst@gmail.com>
2026-07-04 00:05:04 +0100

    methods to build png and jpg

--- app/code_generator.py
+++ app/code_generator.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
+import io
 
 class LaetusCode:
 
@@ -30,6 +32,7 @@ class LaetusCode:
         self.bar_height = self.canvas_height - self.text_size - 2 * self.text_gap
 
         self.code_folder = Path(__file__).resolve().parent / "codes"
+        self.font = str(Path(__file__).resolve().parent / "fonts" / "DejaVuSans-Bold.ttf")
 
     def _to_px(self, mm):
         return int(round(mm*(self.dpi/25.4), 0))
@@ -58,3 +61,53 @@ class LaetusCode:
 
         return laetus_bars, x_bar
     
+    def _create_image(self):
+        canvas_height_px = self._to_px(self.canvas_height)
+        bar_height_px = self._to_px(self.bar_height)
+        bars, canvas_width = self._get_bars()
+        canvas_width_px = self._to_px(canvas_width)
+        img = Image.new("L", (canvas_width_px, canvas_height_px), color="white")
+        draw = ImageDraw.Draw(img)
+
+        for x, width in bars:
+            x = self._to_px(x)
+            width = self._to_px(width)
+            draw.rectangle([x, 0, x + width, bar_height_px], fill="black")
+
+        font = ImageFont.truetype(self.font, self._to_px(self.text_size))
+        text = str(self.code_nr)
+        text_width = draw.textlength(text=text, font=font)
+        x_text = (canvas_width_px - text_width)/2
+        draw.text((x_text, bar_height_px + self._to_px(self.text_gap)), text=text, font=font, fill="black")
+
+        return img
+    
+    def to_jpg(self):
+        code_img = self._create_image()
+        buffer = io.BytesIO()
+        code_img.save(buffer, format="JPEG")
+        buffer.seek(0)
+
+        return buffer
+    
+    def to_png(self):
+        code_img = self._create_image()
+        buffer = io.BytesIO()
+        code_img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        return buffer
+    
+    def to_jpg_file(self):
+        code_img = self._create_image()
+
+        self.code_folder.mkdir(parents=True, exist_ok=True)
+
+        code_img.save(self.code_folder / f"{self.code_nr}.jpg", format="JPEG")
+
+    def to_png_file(self):
+        code_img = self._create_image()
+
+        self.code_folder.mkdir(parents=True, exist_ok=True)
+
+        code_img.save(self.code_folder / f"{self.code_folder}.png", format="PNG")
